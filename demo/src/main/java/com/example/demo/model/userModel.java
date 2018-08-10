ef91995ff8a174775d3e94251aa0bc2ee44115db
unknown <pbgaya1993@gmail.com>
2018-08-10 21:21:31 +0530

    compleat form

---
+++ demo/src/main/java/com/example/demo/model/userModel.java
@@ -0,0 +1,38 @@
+package com.example.demo.model;
+
+public class userModel {
+	
+	private String first_name;
+	private String last_name;
+	
+	
+	
+	public userModel() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	
+	
+	public userModel(String first_name, String last_name) {
+		super();
+		this.first_name = first_name;
+		this.last_name = last_name;
+	}
+
+
+	public String getFirst_name() {
+		return first_name;
+	}
+	public void setFirst_name(String first_name) {
+		this.first_name = first_name;
+	}
+	public String getLast_name() {
+		return last_name;
+	}
+	public void setLast_name(String last_name) {
+		this.last_name = last_name;
+	}
+	
+	
+
+}
